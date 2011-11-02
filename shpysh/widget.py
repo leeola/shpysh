05c9ac61d6589c9fcde376251e747f33f073bf88
Lee Olayvar <leeolayvar@gmail.com>
2011-11-01 19:22:44 -0700

    Added the boiler code.

---
+++ shpysh/widget.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+'''
+    shpysh.widget
+    ~~~~~~~~~~~~~
+
+    The urwid Widgets used in the shpysh gui.
+
+    :copyright: 2011 by Lee Olayvar
+    :license: MIT, see LICENSE for more details
+'''
+
